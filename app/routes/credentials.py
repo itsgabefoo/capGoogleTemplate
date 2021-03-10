b44d4662849c82c61ac4c81194ee725de40610e2
conchesness <37635142+conchesness@users.noreply.github.com>
2021-03-10 14:34:31 -0800

    Set up for Google Cloud Run

---
+++ app/routes/credentials.py
@@ -0,0 +1,18 @@
+import os
+
+GOOGLE_CLIENT_CONFIG = {
+    "web":{
+        "client_id":"472313601389-e8cfie12jisb2v8c8ghkp2pei3vndtou.apps.googleusercontent.com",
+        "project_id":"bullcoin",
+        "auth_uri":"https://accounts.google.com/o/oauth2/auth",
+        "token_uri":"https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs",
+        "client_secret":"Ec7h79AmkBLhu3BufCFgffPP",
+        "redirect_uris":
+            [
+                "https://127.0.0.1:5000/oauth2callback",
+                "https://localhost:5000/oauth2callback",
+                "https://bullcoin.hsoakland.tech/oauth2callback"
+            ]
+        }
+}

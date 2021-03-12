811f9e3228c06dca1cacdf9181c1d1436cec6c47
prinzsammy <eleabedereje@gmail.com>
2021-03-12 09:46:41 -0800

    secret key for local server

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 # this will geberate a random number each tiome this app is run to be the secret key because
 # os.environ.get("SECRET_KEY") will fail because we do not have that variable set in the os.environment
-app.config["SECRET_KEY"] = os.environ.get("FLASK_SECRET_KEY") # or os.urandom(20)
+app.config["SECRET_KEY"] = os.environ.get("FLASK_SECRET_KEY") or os.urandom(20)
 # you must change the next line to be link to your database at mlab
 connect("bullcoin", host='mongodb+srv://admin:bulldogz@cluster0.8m0v1.gcp.mongodb.net/bullcoin?retryWrites=true&w=majority')
 
